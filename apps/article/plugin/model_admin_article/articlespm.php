f923e1b7e5a7034516b991326f1b1ab804424df8
易木百宝 <charles417@gmail.com>
2019-02-26 11:16:32 +0800

    Update articlespm.php
    如果配置文件中没有catid的配置，不检查栏目ID

--- apps/article/plugin/model_admin_article/articlespm.php
+++ apps/article/plugin/model_admin_article/articlespm.php
@@ -28,9 +28,11 @@ class plugin_articlespm extends object
     private function articlespm()
     {
         if($this->article->data['status'] != 6) return; //状态不等于发布时跳出
-        $catids = explode(',', $this->article->category[$this->article->data['catid']]['parentids']);
-        array_push($catids, $this->article->data['catid']);
-        if(!array_intersect($catids, config('articlespm', 'catid'))) return;
+        if(config('articlespm', 'catid')) {
+            $catids = explode(',', $this->article->category[$this->article->data['catid']]['parentids']);
+            array_push($catids, $this->article->data['catid']);
+            if(!array_intersect($catids, config('articlespm', 'catid'))) return;
+        }
         factory::queue('articlespm')->add($this->article->data); //数据加入队列
         return;
     }
